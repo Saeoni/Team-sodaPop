531b0844778e2657427cbc5751f58a9adbe94bd0
Ronald Martz <rjmartz@student.fullsail.edu>
2025-09-07 23:02:51 -0500

    playerController Update
    Added sprint mechanics and placed the player as a prefab.

--- Team sodaPop project/Assets/Scripts/playerController.cs
+++ Team sodaPop project/Assets/Scripts/playerController.cs
@@ -7,26 +7,33 @@ public class playerController : MonoBehaviour
     [SerializeField] int HP;
     [SerializeField] int speed;
     [SerializeField] int sprintMod;
-    [SerializeField] int jumpSpeed;
+    [SerializeField] float jumpSpeed;
+    [SerializeField] float jumpMod;
     [SerializeField] int jumpMax;
     [SerializeField] int gravity;
 
     int HPOrig;
+    int speedOrig;
     int jumpCount;
 
     Vector3 moveDir;
     Vector3 playerVel;
 
+    bool isSprinting;
+    bool isTired = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         HPOrig = HP;
+        speedOrig = speed;
     }
 
     // Update is called once per frame
     void Update()
     {
         movement();
+        sprint();
     }
 
     void movement()
@@ -57,6 +64,23 @@ public class playerController : MonoBehaviour
         {
             jumpCount++;
             playerVel.y = jumpSpeed;
+        }
+    }
+
+    void sprint()
+    {
+        if (Input.GetButtonDown("Sprint"))
+        {
+            speed *= sprintMod;
+            jumpSpeed *= jumpMod;
+            isSprinting = true;
+        }
+
+        if (Input.GetButtonUp("Sprint"))
+        {
+            speed /= sprintMod;
+            jumpSpeed /= jumpMod;
+            isSprinting = false;
         }
     }
 }
