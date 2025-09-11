f63d2c578b8b53613e602ef5e65d1b83f3be8c03
Tobias Young <tobyyoung1994@gmail.com>
2025-09-11 00:09:42 -0400

    Had to update the Player Script it didnt have IDamage attached to it

--- Team sodaPop project/Assets/Scripts/playerController.cs
+++ Team sodaPop project/Assets/Scripts/playerController.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections;
 
-public class playerController : MonoBehaviour
+public class playerController : MonoBehaviour, IDamage
 {
     [SerializeField] CharacterController controller;
 
