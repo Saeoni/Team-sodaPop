751d9922e6a03c43cee409f06583725cc65318e3
Tobias Young <tobyyoung1994@gmail.com>
2025-09-22 14:28:48 -0400

    ReaperAI Script and EnemyAI updates

--- Team sodaPop project/Assets/Scripts/EnemyAI.cs
+++ Team sodaPop project/Assets/Scripts/EnemyAI.cs
@@ -8,71 +8,82 @@ public class EnemyAI : MonoBehaviour, IDamage
     [Header("Core Components")]
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Renderer model;
+    [SerializeField] Animator animator;
     [SerializeField] Transform shootPos;
     [SerializeField] Transform headPos;
     [SerializeField] GameObject bulletPrefab;
     [SerializeField] GameObject keyPrefab;
-    [SerializeField] Animator animator;
     
-
-    [Header("Enemy Settings")]
+    [Header("Detection & Chase Settings")]
     [SerializeField] float detectionRadius;
     [SerializeField] float chaseSpeed;
-    [SerializeField] float shootRate;
     [SerializeField] float faceTargetSpeed;
     [SerializeField] float FOV;
     [SerializeField] LayerMask lineOfSightMask;
+
+    [Header("Combat Settings")]
+    [SerializeField] float shootRate;
     [SerializeField] int HP;
     [SerializeField] int damage;
 
-    
+    [Header("Roaming Settings")]
+    [SerializeField] float roamRadius;
+    [SerializeField] float distToRoam;
+    [SerializeField] float animTransSpeed;
 
-    float shootTimer;
-    float angleToPlayer;
-    bool playerInTrigger;
-    Color colorOrig;
-    Vector3 playerDir;
+    float _shootTimer;
+    float _roamTimer;
+    float _originalStopDist;
+    float _angleToPlayer;
+    bool _playerInTrigger;
+    Color _colorOrig;
+    Vector3 _playerDir;
+    Vector3 _spawnPos;
 
-   
-
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
     void Start()
     {
-        colorOrig = model.material.color;
+        _colorOrig = model.material.color;
         gamemanager.instance.updateGameGoal(1);
-        
+        _spawnPos = transform.position;
+        _originalStopDist = agent.stoppingDistance;
+
+   
     }
 
     // Update is called once per frame
     void Update()
     {
-        shootTimer += Time.deltaTime;
+        _shootTimer += Time.deltaTime;
    
 
-        if (playerInTrigger && canSeePlayer())
+        if (_playerInTrigger && canSeePlayer())
         {
            
         }
        
     }
 
+    void UpdateAnimLocomotionAnim()
+    {
+        float currSpeed = agent.velocity.magnitude;
+        float normalizedSpeed = currSpeed / agent.speed;
+        float animFloat = animator.GetFloat("Speed");
 
-   
-
+        animator.SetFloat("Speed", Mathf.Lerp(animFloat, normalizedSpeed, Time.deltaTime * animTransSpeed));
+    }
 
     bool canSeePlayer()
     {
         if (!gamemanager.instance.isStealthed)
         {
-            playerDir = gamemanager.instance.player.transform.position - headPos.position;
-            angleToPlayer = Vector3.Angle(playerDir, transform.forward);
-            Debug.DrawRay(headPos.position, playerDir, Color.red);
+            _playerDir = gamemanager.instance.player.transform.position - headPos.position;
+            _angleToPlayer = Vector3.Angle(_playerDir, transform.forward);
+            Debug.DrawRay(headPos.position, _playerDir, Color.red);
 
             RaycastHit hit;
-            if (Physics.Raycast(headPos.position, playerDir, out hit))
+            if (Physics.Raycast(headPos.position, _playerDir, out hit))
             {
-                if (angleToPlayer <= FOV && hit.collider.CompareTag("Player"))
+                if (_angleToPlayer <= FOV && hit.collider.CompareTag("Player"))
                 {
                     agent.SetDestination(gamemanager.instance.player.transform.position);
 
@@ -81,16 +92,16 @@ public class EnemyAI : MonoBehaviour, IDamage
                         faceTarget();
                     }
 
-                    if (shootTimer >= shootRate)
+                    if (_shootTimer >= shootRate)
                     {
                         shoot();
                     }
-
+                    agent.stoppingDistance = _originalStopDist;
                     return true;
                 }
             }
         }
-
+        agent.stoppingDistance = 0;
         return false;
     }
 
@@ -98,18 +109,28 @@ public class EnemyAI : MonoBehaviour, IDamage
 
     void faceTarget()
     {
-        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDir.x, transform.position.y, playerDir.z));
+        Quaternion rot = Quaternion.LookRotation(new Vector3(_playerDir.x, transform.position.y, _playerDir.z));
       transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * faceTargetSpeed);
+    }
+
+    void ChaseAndShoot()
+    {
+        Vector3 target = gamemanager.instance.player.transform.position;
+        agent.SetDestination(target);
     }
 
     void shoot()
     {
-        shootTimer = 0;
+        _shootTimer = 0;
+        animator.SetTrigger("Shoot");
+    }
+
+    public void CreateBullet()
+    {
         Instantiate(bulletPrefab, shootPos.position, transform.rotation);
     }
 
-   
-     
+
     void OnEnemyDeath()
     {
         Instantiate(keyPrefab, transform.position, Quaternion.identity);
@@ -127,26 +148,22 @@ public class EnemyAI : MonoBehaviour, IDamage
         }
     }
 
-    
-
     IEnumerator flashRed()
     {
         model.material.color = Color.red;
         yield return new WaitForSeconds(0.1f);
-        model.material.color = colorOrig;
+        model.material.color = _colorOrig;
     }
-
-
     void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-            playerInTrigger = true;
+            _playerInTrigger = true;
     }
 
     void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
-            playerInTrigger = false;
+            _playerInTrigger = false;
     }
 
 }
