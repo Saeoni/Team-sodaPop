33435c667cd45dfcd32f31919e2cbbcf002c714b
Christopher McNeil <lsumcneil.13@gmail.com>
2025-10-21 22:09:18 -0500

    merge and options in turtial

--- Team sodaPop project/Assets/Scripts/buttonFunction.cs
+++ Team sodaPop project/Assets/Scripts/buttonFunction.cs
@@ -4,6 +4,7 @@ using UnityEngine.SceneManagement;
 public class ButtonFunctions : MonoBehaviour
 {
     public string firstLevel;
+    float timeScaleOrig;
 
     public GameObject optionsMenu;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -41,11 +42,20 @@ public class ButtonFunctions : MonoBehaviour
     public void Options()
     {
         optionsMenu.SetActive(true);
+        gamemanager.instance.stateUnpause();
+        timeScaleOrig = Time.timeScale;
+        Time.timeScale = 0;
+        Cursor.visible = true;
+
+        Cursor.lockState = CursorLockMode.None;
     }
 
     public void CloseOptions()
     {
         optionsMenu?.SetActive(false);
+        Time.timeScale = timeScaleOrig;
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 
 }
